65c939a7f1c8caaef04ba07e5ea07da8195715d1
unknown <christianeduardogb@gmail.com>
2015-09-18 14:35:10 -0500

    se añadio Laravel y nueva carpeta chelam

---
+++ chelam/resources/views/header.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Chelam</title>
+	
+    <!-- css -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link href="css/nivo-lightbox.css" rel="stylesheet" />
+	<link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+	<link href="css/animations.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
+	<link href="color/default.css" rel="stylesheet">
+	<link rel="shortcut icon" type="image/png" href="img/chelas/imago_color.png"/>
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+	<!-- Trigger the modal with a button -->
+	
+
+	<!-- Modal -->
+	<div id="myModal" class="modal modal-wide fade in text-center" role="dialog">
+	  <div class="modal-dialog">
+
+	    <!-- Modal content-->
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h4 class="modal-title">¿Eres mayor de edad?</h4>
+	      </div>
+	      <div class="modal-body">
+	      	<div class="container modalImg">
+	      	</div>
+	        <div class="botonesSiNo col-md-12">
+	        	<div class="col-md-6">
+	        		<a class="btn btn-skin btn-scroll" id="btnSi">Si</a>
+	        	</div>
+	        	<div class="col-md-6">
+	        		<a href="https://www.youtube.com/watch?v=k7exgdlVyU0" class="btn btn-skin btn-scroll" id="btnNo">No</a>
+	        	</div>
+	        </div>
+	      </div>
+	    </div>
+
+	  </div>
+	</div>
+	<div id="navigation">
+        <nav class="navbar navbar-custom" role="navigation">
+                              <div class="container">
+                                    <div class="row">
+                                          <div class="col-md-2">
+                                                   <div class="site-logo">
+                                                            <!--<a href="index.html" class="brand">CHELAM</a>-->
+                                                            <a href="/"><img src="img/chelas/logovert_color.png" class="miniLogo"></a>
+                                                    </div>
+                                          </div>
+                                          
+
+                                          <div class="col-md-10">
+                         
+                                                      
+                                          <div class="navbar-header">
+                                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+                                                <i class="fa fa-bars"></i>
+                                                </button>
+                                          </div>
+                                                      
+                                                      <div class="collapse navbar-collapse" id="menu">
+                                                            <ul class="nav navbar-nav navbar-right">
+                                                                  <li class="active"><a href="chelas">Chelas</a></li>
+                                                                  <li><a href="chelapack">Chelapack</a></li>
+																                                  <li><a href="#service">Mayoreo</a></li>
+                                                                  <li><a href="#works">Membresias</a></li>				
+                                                                  <li><a href="#contact">Preguntas Frecuentes</a></li>
+                                                                  <li><a href="login.html">Login</a></li>
+                                                                  <li><a href="#"><span class="fa fa-shopping-cart fa-2x"></span></a></li>
+                                                                  <li><a>0</a></li>
+                                                            </ul>
+                                                      </div>
+                             
+                                          </div>
+                                    </div>
+                              </div>
+                        
+                        </nav>
+    </div> 	
