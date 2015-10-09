c589906c4dafb4280217f8662329bb952b0f34f3
unknown <christianeduardogb@gmail.com>
2015-10-08 23:53:09 -0500

    Usuario vista creada y mejorada

--- chelam/resources/views/user.blade.php
+++ chelam/resources/views/user.blade.php
@@ -1,49 +1,60 @@
 @include('header')
-<div id="perfil">
-	<img src="img/user1.jpg">
-</div>
 
-<div id="data_user">
-	<h5>yelow melo</h5>
-	<hr>
-	<h2>Datos de Compra</h2>
-	<a href="#">Cambiar contraseña</a>
-</div>
+<section id="service" class="home-section color-dark bg-gray">
+	<div class="text-center">
+		<div class="container">
+			<div class="row ">
+		        <div class="col-xs-6 col-sm-4 col-md-4">
+					<div class="">
+		            <div >
+						<div class="service-icon">
+							<div class="comoImg1">
+								<div class="imgUser" id="imgSesion"></div>
+							</div>
+						</div>
+						<div class="service-desc">
+							<h3 class="nombreUser"><?php echo \Session::get("nameUsuarioSesion") ?></h3>
+							<div class="divider-header"></div>
+							<p>
+							<h5>Datos de compra</h5> <br><a href="#">Cambiar Contraseña.</a>
+							</p>
+						</div>
+		            </div>
+					</div>
+		        </div>
+		        <div class="col-xs-6 col-sm-4 col-md-8">
+					
+	            	<h4 class="hdCompras">HISTORIAL DE COMPRAS</h4>
+	            	<div class="service-desc">
+	            		<div class="divider-header"></div>
+	            	</div>
+					<div class="service-desc">
+						<table class="table table table-hover">
+							<thead>
+								<th>Fecha</th>
+								<th>Producto</th>
+								<th>Costo</th>
+								<th>Estatus</th>
+							</thead>
+							<tr>
+								<td class="textRowCenter">01/09/2015</td>
+								<td class="tdLeft">
+									<ul>
+										<li><b>Tzotzil</b> Lager 1x $65.00</li>
+										<li><b>Amnesia</b> Nocturna 1x $65.00</li>
+										<li><b>Apophis</b> Imperial Stout 1x $65.00</li>
+										<li><b>Tzotzil</b> Ambar 1x $65.00</li>
+									</ul>
+								</td>
+								<td class="ui-helper-center"><h5>$390.00</h5></td>
+								<td class="textRowCenter"><i>Entregado</i></td>
+							</tr>
+						</table>		
+					</div>
+		        </div>
+		    </div>
+		</div>
+	</div>
+</section>
 
-<div id="table">
-	<table class="table table-bordered">
-	<thead>
-		<tr>
-			<th>Fecha</th>
-			<th>Producto</th>
-			<th>Costo</th>
-			<th>Estatus</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<th>01/09/2015</th>
-			<th>
-				<ul>
-					<li>Tzotzil </li>
-					<li>Tzotzil</li>
-					<li>Tzotzil</li>
-					<li>Tzotzil</li>
-				</ul>
-			</th>
-			<th>$ 390.00</th>
-			<th>Entregado</th>
-		</tr>
-		<tr>
-			<th>05/09/2015</th>
-			<th>
-				<ul>
-					<li>ChelaPack</li>
-				</ul>
-			</th>
-			<th>$ 490.00</th>
-			<th>Pendiente</th>
-		</tr>
-  </table>
-</div>
 @include('footer')
